25e8ec9eb36132e538efa3f164be1a291efb8f95
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-04-30 19:31:23 -0400

    add call for artist albums. figure out how to return though

--- src/data/extract_spotify.py
+++ src/data/extract_spotify.py
@@ -2,27 +2,33 @@
 This module calls the Spotify API through Python driver Spotipy and parses
 results for information relevant to this study.
 """
-
 import spotipy
-import config
+from config import *
+from consts import *
 
 
 def get_auth():
+	"""
+	Attempts to get a Spotify API authentication token.
 
+	Returns
+	-------
+	A spotify.Spotify client object if a token is retrieved. Else, gives a
+	failure message.
+	"""
 	token = spotipy.util.prompt_for_user_token(
-		username = config.username,
-		client_id = config.client_id,
-		client_secret = config.client_secret,
-		redirect_uri = config.redirect_uri
+		username = username,
+		client_id = client_id,
+		client_secret = client_secret,
+		redirect_uri = redirect_uri
 	)
-
 
 	if token:
 		sp = spotipy.Spotify(auth=token)
 		return sp
 
 	else:
-		print("Can't get token for", config.username)
+		print("Can't get token for", username)
 
 def get_artist_id(artist, sp):
 	"""
@@ -32,7 +38,7 @@ def get_artist_id(artist, sp):
 
 	Parameters
 	----------
-	artist [str]: The artist name 
+	artist [str]: The artist name
 
 	sp [spotipy.Spotify obj]: The currently active Spotify client object.
 	"""
@@ -40,10 +46,44 @@ def get_artist_id(artist, sp):
 	items = results['artists']['items']
 	if len(items):
 		artist = items[0]
-		print(artist)
+		return artist['uri']
+
+def get_artist_albums(artist_id, sp):
+	"""
+	"""
+	# Can change any of these params
+	print(
+		sp.artist_albums(
+			artist_id, album_type='album', country=None, limit=50, offset=0
+		)
+	)
+
+
+def get_table(json):
+	"""
+	Helper for get_artist_albums. Parses the JSON received from the
+	artist_albums API call.
+	"""
+	table = {
+		'artist_name': [],
+		'album_name': [],
+		'album_id': [],
+		'release_date': [],
+		'total_tracks': []    
+	}
+
+	for item in json['items']:
+		table['artist_name'].append(item['artists'][0]['name'])
+		table['album_name'].append(item['name'])
+		table['album_id'].append(item['uri'])
+		table['release_date'].append(item['release_date'])
+		table['total_tracks'].append(item['total_tracks'])
 
 
 
 if __name__ == '__main__':
-	# get_auth()
-	sp = spotipy.Spotify(auth = config.token)
+
+	sp = get_auth()
+	artist_id = get_artist_id(artist, sp)
+	get_artist_albums(artist_id, sp)
+
